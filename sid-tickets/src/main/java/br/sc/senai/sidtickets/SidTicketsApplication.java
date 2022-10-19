7efe07f2c1884ec9429b16c262c7f6e618429688
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-10-19 19:17:28 -0300

    Push project files

---
+++ sid-tickets/src/main/java/br/sc/senai/sidtickets/SidTicketsApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senai.sidtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidTicketsApplication.class, args);
+	}
+
+}
---
+++ sid-tickets/src/test/java/br/sc/senai/sidtickets/SidTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senai.sidtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
